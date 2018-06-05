03fb4bd88f611bde247cf96fc796430327b4925a
Maxime Duchene <duchene.maxime@outlook.fr>
2018-06-05 17:03:52 +0200

    tp 2.3 terminé, utilisation de Babel

---
+++ pub-services/src/index.js
@@ -0,0 +1,18 @@
+var pubService = require('./services/pubs.services.js');
+
+function getListPub() {
+    console.log(pubService.tousPubs());
+}
+
+function getListPubOpen() {
+    console.log(pubService.pubsOuverts());
+};
+
+console.log(pubService.pubsOuverts());
+
+module.exports = {
+    services: {
+        getListPub: getListPub,
+        getListPubOpen: getListPubOpen
+    }
+}
